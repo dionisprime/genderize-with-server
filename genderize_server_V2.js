2c5537b1cd635ed6e2b946dcad67dc2cfc9a4879
Dionis Prime <dioniskruspe@gmail.com>
2023-09-16 13:22:47 +0300

    улучшение кода

--- genderize_server_V2.js
+++ genderize_server_V2.js
@@ -6,25 +6,28 @@ server.on('request', async (req, res) => {
         let userName =
             req.url === '/favicon.ico' ? res.end() : req.url.substring(1);
 
-        await checkGender(userName, res);
+        const result = await checkGender(userName);
+
+        res.end(result);
     } catch (error) {
         console.error('Error: ', error.message);
+        res.end(error.message);
     }
 });
 
-async function checkGender(name, res) {
+async function checkGender(name) {
     const data = await getGenderInfoByName(name);
     if (data.gender === null) {
         console.log(`The gender for "${data.name}" could not be determined.`);
-        res.end(`The gender for "${data.name}" could not be determined.`);
+        return `The gender for "${data.name}" could not be determined.`;
     } else {
         console.log(`${name} is ${data.gender}.`);
-        res.end(`${data.name} is ${data.gender}`);
+        return `${data.name} is ${data.gender}`;
     }
 }
 
 async function getGenderInfoByName(name) {
-    const serverUrl = 'https://api.genderize.io';
+    const serverUrl = 'https://api.genderizes.io';
     const url = `${serverUrl}?name=${name}`;
     const response = await fetch(url);
     return (await response.json()) || {};
